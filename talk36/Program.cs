c7efe070e0b2a9035422d732fca6da00d9a3e960
David <80980510+kreofox@users.noreply.github.com>
2022-12-18 15:02:50 +0600

    5

--- talk36/Program.cs
+++ talk36/Program.cs
@@ -1,30 +1,14 @@
-﻿Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-int sum = 0;
-
-for (int z = 0; z < numbers.Length; z+=2)
-    sum = sum + numbers[z];
-
-    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
-
-void FillArrayRandomNumbers(int[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = new Random().Next(1,10);
-        }
-}
-void PrintArray(int[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
-}
+var rnd = new Random();
+var arr = new int[1];
+ 
+for (int i = 0; i < arr.Length; i++)
+    Console.Write("{0}\t", arr[i] = rnd.Next(-100, 100));
+Console.WriteLine();
+ 
+var even = arr.Where((element, index) => index % 2 != 0);
+var odd = arr.Where((element, index) => index % 2 == 0);
+ 
+Console.WriteLine("Чётные. Количество: {0}. Сумма: {1}.", even.Count(), even.Sum());
+Console.WriteLine("Нечётные. Количество: {0}. Сумма: {1}.", odd.Count(), odd.Sum());
+ 
+Console.ReadKey();
